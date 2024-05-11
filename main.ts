66c7fa9510e34d57424c1c1a40a39105e402288c
toobasaleem123 <166385442+toobasaleem123@users.noreply.github.com>
2024-05-10 21:24:19 -0700

    first commit

---
+++ main.ts
@@ -0,0 +1,33 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+let todos=[]
+let condition="true"
+
+console.log(chalk.yellowBright.bold("\n \t Welcome to code with tooba--to do list \n"))
+
+while(condition)
+{    
+ let addTask=await inquirer.prompt([
+    {
+        name:"task",
+        type:"input",
+        message:chalk.bgBlue("Enter your New Task..")
+    }
+ ])
+ todos.push(addTask.task)
+
+ console.log(`${addTask.task},task added in your list`)
+ let moreTask=await inquirer.prompt([
+    {
+        name:"NewTask",
+        type:"confirm",
+        message:chalk.bgBlue("Do you want to add more Task??"),
+        default:"false"
+    }
+ ])
+ condition = moreTask.NewTask
+}
+
+console.log("Your new To Do list is:",{todos})
